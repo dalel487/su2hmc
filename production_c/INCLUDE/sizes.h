56d49c66838767a12f3b46469fa9059caad84e1a
Dale Lawlor <dale.lawlor.2021@mumail.ie>
2021-06-29 13:45:51 +0100

    Final checks to Intel Clang makefile. It's crap at the moment but will probably get better in the long run. Plus since AMD's optimising compiler is CLANG based it isn't a complete waste of time to have one working

--- production_c/INCLUDE/sizes.h
+++ production_c/INCLUDE/sizes.h
@@ -34,8 +34,8 @@
 #define	FILELEN	64
 // Common block definition for parallel variables
 
-#define	nx	4
-#define	nt	8
+#define	nx	8
+#define	nt	16
 
 // Keep original restriction of single spatial extent
 
