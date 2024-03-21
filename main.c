153bf46a40cee97b3a3755c03fc75c10ad88511f
Dale Lawlor <dalel487@thphys.nuim.ie>
2024-03-21 19:29:33 +0000

    Main fix is in main.c around line 460ish. We convert a complex array from single to double precision, but the GPU was calling the code to convert a Real array between precisions, so half the elements were ignored. Currently testing to see if results work out correctly.

--- main.c
+++ main.c
@@ -425,6 +425,7 @@ int main(int argc, char *argv[]){
 			Complex_f *R=aligned_alloc(AVX,kfermHalo*sizeof(Complex_f));
 			memset(R1_f,0,kferm*sizeof(Complex_f));
 #endif
+			Complex_f *R=aligned_alloc(AVX,kfermHalo*sizeof(Complex_f));
 			//The FORTRAN code had two Gaussian routines.
 			//gaussp was the normal Box-Muller and gauss0 didn't have 2 inside the square root
 			//Using σ=1/sqrt(2) in these routines has the same effect as gauss0
@@ -440,7 +441,6 @@ int main(int argc, char *argv[]){
 			cudaMemPrefetchAsync(R,kfermHalo*sizeof(Complex_f),device,NULL);
 			cudaDeviceSynchronise();
 #endif
-			Dslashd_f(R1_f,R,u11t_f,u12t_f,iu,id,gamval_f,gamin,dk4m_f,dk4p_f,jqq,akappa);
 #ifdef _DEBUG
 #ifdef __NVCC__
 			//Make sure the multiplication is finished before freeing its input!!
@@ -459,7 +459,6 @@ int main(int argc, char *argv[]){
 #endif
 			//cudaFree is blocking so don't need cudaDeviceSynchronise()
 #else
-			free(R); 
 #pragma omp simd aligned(R1_f,R1:AVX)
 			for(int i=0;i<kferm;i++)
 				R1[i]=(Complex)R1_f[i];
