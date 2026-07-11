d6360275b77f03eaccd9149704e21b91f4a438bf
Dale Lawlor <dalel487@thphys.nuim.ie>
2026-07-11 21:32:40 +0100

    Hipify and Makefile script for HIP. Still work in progress

---
+++ production_hip/Hipify.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+#Step 1: Convert the CUDA files to HIP. Basically replaces cu or cuda with HIP in all CUDA standard functions, and
+#replaces __CUDACC__ with __HIPCC__
+shopt -s nullglob
+prehips=( ../production_cuda/*.prehip ../production_c/*.prehip ../INCLUDE/*.prehip )
+[[ -e ../main.c.prehip ]] && prehips+=( ../main.c.prehip )
+for f in "${prehips[@]}"; do mv -- "$f" "${f%.prehip}"; done
+
+hipify-perl --print-stats --inplace ../production_cuda/*.cu ../INCLUDE/*.h ../production_c/*.c ../main.c
+
+#Step 2: Remove some over-zealous headers from the pure C code paths
+perl -ni -e 'print unless m{^\s*#include\s*[<"]hip/hip_runtime\.h[">]}' \
+  ../production_c/*.c ../main.c ../INCLUDE/*.h 
+ 
+
